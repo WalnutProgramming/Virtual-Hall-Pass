59de67f18fd10cf7b4712305369cf53017ed9748
Bob Beebo <>
2023-01-06 00:31:38 +0000

    initial commit

---
+++ backup.py
@@ -0,0 +1,39 @@
+import os
+import time
+
+time_now = time.ctime()
+day_time = time.gmtime().tm_yday
+
+
+def cleanup_backups():  #Delete backups older than 5 days
+    for b in os.listdir("./backups"):
+        for i in os.listdir(f"./backups/{b}"):
+          try:
+              day = int(i[0])
+          except:
+              continue
+          if day <= day_time - 5 or day >= day_time + 5:
+              os.remove(f"./backups/{i}")
+              print("Removed backup older than five days")
+    print("Finished backup cleanup")
+
+
+def backup(file):  #Backup file based on current day of the year
+    print("Creating backup")
+    filefoldername = file.split(".")[0]
+    with open(f"./{file}", "r") as e:
+        cont = e.read()
+    cont = f"BackupTime = '{time_now}'#Automatically generated backup time\n" + cont
+    if not os.path.exists("./backups"):
+        os.mkdir("./backups")
+    if not os.path.exists(f"./backups/{filefoldername}"):
+      os.mkdir(f"./backups/{filefoldername}")
+    with open(f"./backups/{filefoldername}/{day_time}{file}", "w") as e:
+      print("passing")
+      e.write(cont)
+    print(f"Backup saved in ./backups/{filefoldername}/{day_time}{file}")
+
+
+backup("main.py")
+backup("Token.py")
+cleanup_backups()
---
+++ plan.md
@@ -0,0 +1,30 @@
+## To access Designs/Wireframes
+1. Create a Figma account if you haven't already: https://www.figma.com/signup
+
+2. Join the WHHS Programming group: https://www.figma.com/team_invite/redeem/NaKnykNEtQ2v7NeD3xpSiL
+  
+3. View and implement the wireframe linked below (or find through the Figma group):
+https://www.figma.com/files/project/68320862/Hall-Pass?fuid=1079194947883687021
+
+# To Do
+- Add encryption to token handler
+
+
+## Add HTML/CSS for other pages (based off designs)
+- Profile
+- Classes
+- About
+- Pass in Progress
+
+## Add Flask Responsiveness from Hall Pass Mockup
+Mockup link: https://replit.com/@susthebus/Hall-Pass#main.py
+- Database?
+
+## Teacher/Administrator Backend Page
+- backend.html
+- Authentication?
+
+## Comments!!
+- Not a priority but obviously very convenient and useful
+
+ ## Backup (done)
